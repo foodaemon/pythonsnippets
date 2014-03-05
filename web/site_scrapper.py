b2cb5746e803502017e6a8d0607d5c498815263c
Robin Tiwari <robin.tiwari@ayokasystems.com>
2014-03-05 17:50:56 -0600

    updated code to python site scrapper snippet

--- web/site_scrapper.py
+++ web/site_scrapper.py
@@ -1,1 +1,21 @@
-__author__ = 'robin.tiwari'
+"""
+Python snippet to scrape url
+Requirements: requests, beautifulsoup4. To install run "pip install requests" and "pip install beautifulsoup4"
+"""
+import requests     # pip install requests
+from bs4 import BeautifulSoup, SoupStrainer     # pip install beautifulsoup4
+
+
+def get_urls(url):
+    request = requests.get(url)     # make get request
+    page = request.text
+
+    soup = BeautifulSoup(page, parse_only=SoupStrainer('a'))
+    for tag in soup:
+        if tag.has_attr('href'):
+            print(tag['href'])
+
+"""
+calls the get_urls method
+"""
+get_urls("http://url_that_you_are_trying_to_scrap.com")
