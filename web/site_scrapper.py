a3dc3a3bf87912d577107c2f851301ac5dc55384
Robin <nixdaemon@users.noreply.github.com>
2014-10-02 13:00:12 -0500

    added argument parser functionality

--- web/site_scrapper.py
+++ web/site_scrapper.py
@@ -2,12 +2,12 @@
 Python snippet to scrape url
 Requirements: requests, beautifulsoup4. To install run "pip install requests" and "pip install beautifulsoup4"
 """
-import requests     # pip install requests
-from bs4 import BeautifulSoup, SoupStrainer     # pip install beautifulsoup4
-
+import sys, optparse
+import requests
+from bs4 import BeautifulSoup, SoupStrainer
 
 def get_urls(url):
-    request = requests.get(url)     # make get request
+    request = requests.get(url)
     page = request.text
 
     soup = BeautifulSoup(page, parse_only=SoupStrainer('a'))
@@ -15,7 +15,21 @@ def get_urls(url):
         if tag.has_attr('href'):
             print(tag['href'])
 
-"""
-calls the get_urls method
-"""
-get_urls("http://url_that_you_are_trying_to_scrap.com")
+
+def main():
+    url = ''
+    usage = "usage: %prog <url>"
+    parser = optparse.OptionParser(usage)
+    parser.add_option("-v", "--verbose", action="store_true", dest="verbose")
+
+    (options, args) = parser.parse_args()
+
+    if len(args) != 1:
+        parser.error("incorrect number of args")
+    url = args[0]
+    if options.verbose:
+        print("Downloading page {0}".format(url))
+    get_urls(url)
+
+if __name__ == "__main__":
+    main()
